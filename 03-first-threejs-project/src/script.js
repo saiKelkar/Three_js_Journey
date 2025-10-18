329de98be6f00ab727ba1fddaeb80b32b5a8b6a7
saiKelkar <104817425+saiKelkar@users.noreply.github.com>
2025-10-18 11:21:51 +0530

    Update Three.js scene background color

--- 03-first-threejs-project/src/script.js
+++ 03-first-threejs-project/src/script.js
@@ -14,6 +14,10 @@
  * npm run dev
  * (build command - npm run build - it will output the final version of the website)
  * npm install three (for threejs)
+ * 
+ * To work with a starter file directly:
+ * npm install (inside the folder)
+ * and npm run dev (to start the server as before)
  */
 
 // To import threejs
@@ -39,6 +43,8 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+// we need to add the object to the scene
 scene.add(mesh)
 
 /**
