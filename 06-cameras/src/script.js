8f1bee20b47cb2dbc39c130783aa91330ada6c33
saiKelkar <104817425+saiKelkar@users.noreply.github.com>
2025-10-22 10:31:30 +0530

    Update script.js
    Using orthographic, perspective camera, and working example of orbit controls

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,4 +1,14 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+/**
+ * Cursor
+ */
+const cursor = { x: 0, y: 0 }
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
 
 /**
  * Base
@@ -23,12 +33,23 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+
+/*
+// Orthographic Camera -- we need to set the aspect ratio, otherwise the object looks squished
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+*/
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,6 +66,17 @@ const tick = () =>
 
     // Update objects
     mesh.rotation.y = elapsedTime;
+
+    /*
+    // Update camera
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5
+    camera.lookAt(mesh.position)
+    */
+
+    // Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
